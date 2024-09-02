eb445832d257179d8590b1e2c09b34c9ec1091c7
stek765 <stefanozanolli765@gmail.com>
2024-09-02 17:41:59 +0200

    script iniziale

---
+++ CaloryCounter/setup_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Creazione dell'ambiente virtuale Python con Python 3.12
+python3.12 -m venv myenv
+
+# Esegui direttamente il comando per attivare l'ambiente virtuale 
+# in modo che persista nella shell corrente
+source myenv/bin/activate
+
+# Verifica che l'ambiente virtuale sia stato attivato
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Ambiente virtuale attivato correttamente."
+
+    # Esecuzione di npm install per installare le dipendenze Node.js
+    npm install
+
+    echo "Setup completato con successo!"
+else
+    echo "Errore: Ambiente virtuale non attivato correttamente."
+fi
